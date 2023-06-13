e853002830e243f5b956e0fb91cf894058ba221a
AliBaig-xD <69807734+AliBaig-xD@users.noreply.github.com>
2023-06-14 04:25:51 +0500

    Optimised api wrappers

--- ApiWrapper.js
+++ ApiWrapper.js
@@ -1,91 +1,56 @@
-// GET request
-export const getAPI = async (url, query = null, withAuth = false) => {
-    withAuth ? await setApiHeader() : unSetApiHeader();
-    try {
-        const res = await axios.get(url, {
-            params: query
-        })
-        return { data: res, error: {}, isError: false }
-    } catch (err) {
-        return {
-            error: err.response.data,
-            data: {},
-            isError: true
-        }
-    }
-}
+import axios from "axios";
+import { getAccessToken } from "./asyncStorage";
 
-// PUT Request
-export const putAPIWrapper = async (url, body, isFormData = false) => {
-    await setApiHeader();
-    isFormData ? await setFormDataHeader() : null
-    try {
-        const res = await axios.put(url, body)
-        return { data: res, error: {}, isError: false }
-    } catch (err) {
-        return {
-            error: err.response.data,
-            data: {},
-            isError: true
-        }
-    }
-}
-
-// PATCH Request
-export const patchAPIWrapper = async (url, body, withAuth = true) => {
-    withAuth ? await setApiHeader() : unSetApiHeader();
-    try {
-        const res = await axios.patch(url, body)
-        return { data: res, error: {}, isError: false }
-    } catch (err) {
-        return {
-            error: err.response.data,
-            data: {},
-            isError: true
-        }
-    }
-}
-
-// DELETE API WRAPPER
-export const deleteAPIWrapper = async url => {
-    await setApiHeader()
-    try {
-        const res = await axios.delete(url)
-        return { data: res, error: {}, isError: false }
-    } catch (err) {
-        return {
-            error: err.response.data,
-            data: {},
-            isError: true
-        }
-    }
-}
-
-// POST API WRAPPER
-export const postAPIWrapper = async (url, body, withAuth = false, isFormData = false) => {
-    withAuth ? await setApiHeader() : unSetApiHeader();
-    isFormData ? await setFormDataHeader() : null;
-    
-    try {
-        const res = await axios.post(url, body)
-        return { data: res, error: {}, isError: false }
-    } catch (err) {
-        return {
-            error: err.response.data,
-            data: {},
-            isError: true
-        }
-    }
-}
-
-const unSetApiHeader = () => {
-    delete axios.defaults.headers.common.Authorization
-}
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 const setApiHeader = async () => {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + ('USER_TOKEN_HERE')
-}
+  axios.defaults.headers.common.Authorization = `Bearer ${await getAccessToken()}`;
+};
 
-const setFormDataHeader = async () => {
-    axios.defaults.headers.common['Content-Type'] = 'multipart/form-data'
-}
+const unSetApiHeader = () => {
+  delete axios.defaults.headers.common.Authorization;
+};
+
+const handleRequest = async (
+  method,
+  url,
+  body = null,
+  config = {},
+  withAuth
+) => {
+  if (withAuth) {
+    await setApiHeader();
+  } else {
+    unSetApiHeader();
+  }
+  try {
+    const res = await axios.request({
+      method,
+      url,
+      data: body,
+      ...config
+    });
+    return { data: res.data.data, isError: false };
+  } catch (err) {
+    return {
+      error: err.response.data,
+      isError: true
+    };
+  }
+};
+
+export const POST = async (url, body, config, withAuth = true) => {
+  return handleRequest("post", url, body, config, withAuth);
+};
+
+export const PATCH = async (url, body, withAuth = true) => {
+  return handleRequest("patch", url, body, withAuth);
+};
+
+export const GET = async (url, query = null, withAuth = true) => {
+  return handleRequest("get", url, null, { params: query }, withAuth);
+};
+
+export const DELETE = async (url, withAuth = true) => {
+  return handleRequest("delete", url, withAuth);
+};
